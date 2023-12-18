a48a4c4c6696a6932e513b2201a2d76457515521
MicheleSerafini <serafini.michele01@gmail.com>
2023-12-18 17:32:20 +0100

    FEAT > CRUD store()

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Comic;
 use App\Http\Requests\StoreComicRequest;
 use App\Http\Requests\UpdateComicRequest;
+use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
@@ -31,5 +32,24 @@ class ComicController extends Controller
     public function create()
     {
         return view('comics.create');
+    }
+
+    /**
+     * Store the new comic
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $new_comic = new Comic();
+        $new_comic->title = $data['title'];
+        $new_comic->thumb = $data['thumb'];
+        $new_comic->price = $data['price'];
+        $new_comic->series = $data['series'];
+        $new_comic->sale_date = $data['sale_date'];
+        $new_comic->type = $data['type'];
+        $new_comic->description = $data['description'];
+        $new_comic->save();
+
+        return redirect()->route('comics.show', $new_comic);
     }
 }
