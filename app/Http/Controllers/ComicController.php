ed05d3bdba2a25ea1d08a6ca9fbfec2091d6d13b
MicheleSerafini <serafini.michele01@gmail.com>
2023-12-18 14:57:33 +0100

    ADD > skeleton setup + draft of comics migration,  seeder and controller

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comic;
+use App\Http\Requests\StoreComicRequest;
+use App\Http\Requests\UpdateComicRequest;
+
+class ComicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comic $comic)
+    {
+        //
+    }
+}
