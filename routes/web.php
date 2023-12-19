26d7bafe5aa7c93c6d16c59390a311e6139c16aa
MicheleSerafini <serafini.michele01@gmail.com>
2023-12-19 14:37:16 +0100

    ADD > Redirect Route for root request

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return redirect()->route('comics.index');
+});
+
 Route::get('/comics', [ComicController::class, 'index'])
     ->name('comics.index');
 
