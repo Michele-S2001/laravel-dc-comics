1388b8e08515b8f09eb6ffdf8218128f69cec85c
MicheleSerafini <serafini.michele01@gmail.com>
2023-12-19 11:20:27 +0100

    ADD > Fillable prop. in th comic model to allow a mass association of store

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -40,16 +40,17 @@ class ComicController extends Controller
     public function store(Request $request)
     {
         $data = $request->all();
-        $new_comic = new Comic();
-        $new_comic->title = $data['title'];
-        $new_comic->thumb = $data['thumb'];
-        $new_comic->price = $data['price'];
-        $new_comic->series = $data['series'];
-        $new_comic->sale_date = $data['sale_date'];
-        $new_comic->type = $data['type'];
-        $new_comic->description = $data['description'];
-        $new_comic->save();
+        // $new_comic = new Comic();
+        // $new_comic->title = $data['title'];
+        // $new_comic->thumb = $data['thumb'];
+        // $new_comic->price = $data['price'];
+        // $new_comic->series = $data['series'];
+        // $new_comic->sale_date = $data['sale_date'];
+        // $new_comic->type = $data['type'];
+        // $new_comic->description = $data['description'];
+        // $new_comic->save();
+        $new_comic = Comic::create($data);
 
-        return redirect()->route('comics.show', $new_comic);
+        return redirect()->route('comics.show', $new_comic->id);
     }
 }
